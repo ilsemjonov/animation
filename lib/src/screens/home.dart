34cdca5aefe16180d2f5688a2e046032022586cb
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-14 15:11:11 +0300

    stateful widget for animation

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  Widget build(context) {
+
+  }
+}
